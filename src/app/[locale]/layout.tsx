9e32936d1e30aeeb70764420cc89f578ecd87202
Mickael <mickael.mesloub@gmail.com>
2025-03-13 15:17:51 +0100

    chore: edit alias config and fix imports

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
-import { routing } from "i18n/routing";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import "./globals.css";
+import { routing } from "@/i18n/routing";
 
 export default async function LocaleLayout({
   children,
