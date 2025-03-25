2369789859d9446c03084fd42b14d746c3981302
Mickael <mickael.mesloub@gmail.com>
2025-03-25 19:11:14 +0100

    refactor(Theme switch button): use new utils functions to get button Icon, class + create function to get button title

--- src/hooks/useThemeSwitch.ts
+++ src/hooks/useThemeSwitch.ts
@@ -1,16 +1,18 @@
+import { ThemeEnum } from "@/enums/theme";
+import { getCurrentTheme } from "@/utils/theme.utils";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export const useThemeSwitch = () => {
-  const [mounted, setMounted] = useState(false);
-
+  const [mounted, setMounted] = useState<boolean>(false);
   const { setTheme, resolvedTheme } = useTheme();
-  const currentTheme = resolvedTheme === "dark" ? "light" : "dark";
+
+  const currentTheme: ThemeEnum = getCurrentTheme(resolvedTheme);
 
   useEffect(() => setMounted(true), []);
 
   const handleSwitchTheme = () => {
-    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+    setTheme(resolvedTheme === "dark" ? ThemeEnum.LIGHT : ThemeEnum.DARK);
   };
 
   return { mounted, currentTheme, handleSwitchTheme, resolvedTheme };
