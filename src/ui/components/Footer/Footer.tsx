374cc4090d6638aba59165bd74e3fa72391f0f72
Mickael <mickael.mesloub@gmail.com>
2025-04-10 15:52:36 +0200

    style(Footer): edit display of elements on mobile screens

--- src/ui/components/Footer/Footer.tsx
+++ src/ui/components/Footer/Footer.tsx
@@ -4,7 +4,9 @@ import FooterNavSection from "@/ui/components/Footer/components/FooterNavSection
 const Footer: React.FC = () => {
   return (
     <footer className="flex flex-col py-8 px-4">
-      <FooterNavSection />
+      <div className="flex flex-col items-center gap-4 sm:flex-row sm:justify-between sm:px-4">
+        <FooterNavSection />
+      </div>
       <FooterCopyrights />
     </footer>
   );
