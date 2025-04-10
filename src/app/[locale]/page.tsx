9ee64114048239cdbb31f5393b67d0f733f5a893
Mickael <mickael.mesloub@gmail.com>
2025-04-10 15:51:53 +0200

    clean(main page): remove buttons

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,6 @@
 import { Locale, useTranslations } from "next-intl";
 import type { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
-import { Button } from "@/ui/components/Button/Button";
-import { ChevronRight, Loader2, Moon } from "lucide-react";
 
 interface HomePageProps {
   params: Promise<{ locale: Locale }>;
@@ -44,65 +42,7 @@ export default function HomePage() {
           <p className="text-accent font-bold text-xs">En savoir plus</p>
         </div>
       </div>
-
-      <div className="pt-2" />
-
-      <div className="p-2">
-        <Button size="lg">Default</Button>
-      </div>
-      <div className="p-2">
-        <Button size="lg" variant="outline">
-          Outline
-        </Button>
-      </div>
-
-      <div className="pt-2" />
-
-      <div className="p-2">
-        <Button>Default</Button>
-      </div>
-      <div className="p-2">
-        <Button variant="outline">Outline</Button>
-      </div>
-
-      <div className="pt-2" />
-
-      <div className="p-2">
-        <Button size="sm">Default</Button>
-      </div>
-      <div className="p-2">
-        <Button size="sm" variant="outline">
-          Outline
-        </Button>
-      </div>
-
-      <div className="pt-2" />
-
-      <div className="p-2">
-        <Button variant="ghost">Ghost</Button>
-      </div>
-
-      <div className="pt-2" />
-
-      <div className="p-2">
-        <Button variant="outline" size="icon">
-          <ChevronRight />
-        </Button>
-        <Button variant="base" size="icon">
-          <Moon />
-        </Button>
-      </div>
-      <div className="p-2">
-        <Button variant="outline" size="sm">
-          Button with icon <ChevronRight />
-        </Button>
-      </div>
-      <div className="p-2">
-        <Button disabled size="sm">
-          <Loader2 className="animate-spin" />
-          Loading...
-        </Button>
-      </div>
+      <div className="pt-8"></div>
     </div>
   );
 }
